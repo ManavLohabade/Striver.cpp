57820134622c192ad4341ea72dbf5ee03133fc4a
ManavLohabade <manavlohabade26@gmail.com>
2024-10-01 01:13:47 +0530

    Uprgeaded Question 2

--- Question2.cpp
+++ Question2.cpp
@@ -7,3 +7,34 @@ e. 60 to 79 - B
 f. 80 to 100 - A
 Ask user to enter marks and print the corresponding grade.*/
 
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int marks;
+    cin >> marks;
+
+    if(marks < 25) {
+        cout << "F";
+    }
+    else if(marks <= 44) {
+        cout << "E";
+    }
+    else if(marks <= 49) {
+        cout << "D";
+    }
+    else if(marks <= 59) {
+        cout << "C";
+    }
+    else if(marks <= 79) {
+        cout << "B";
+    }
+    else if(marks <= 100) {
+        cout << "A";
+    } 
+    else {
+        cout << "Invalid marks";
+    }
+
+    return 0;
+}
